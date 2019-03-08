f443d3c385d2668274adad7bd439f8a4326f18ca
sudheer <30334083+sudheer0222@users.noreply.github.com>
2019-03-08 16:35:42 +0530

    modified with the spaces in the script

--- rds-instance-data.py
+++ rds-instance-data.py
@@ -6,13 +6,13 @@ import argparse
 
 
 def list_instances():
-   client = boto3.client('rds')
-   instances = client.describe_db_instances() 
-   columns_format="%-3s %-20s %-60s %-12s %-7s %-8s %-8s %-12s"
-   print ( columns_format % ("num", "Identifier", "Endpoint Address", "Class", "Engine", "Version", "MultiAZ","Status"))
-   num = 1
-   for n in range(len(instances.get('DBInstances'))):
-      print ( columns_format % (
+    client = boto3.client('rds')
+    instances = client.describe_db_instances() 
+    columns_format="%-3s %-20s %-60s %-12s %-7s %-8s %-8s %-12s"
+    print (columns_format % ("num", "Identifier             ", "Endpoint Address                                                ", "Class      ", "Engine", "Version", "MultiAZ","Status"))
+    num = 1
+    for n in range(len(instances.get('DBInstances'))):
+       print ( columns_format % (
                                num,
                                instances.get('DBInstances')[n].get('DBInstanceIdentifier'),
                                instances.get('DBInstances')[n].get('Endpoint').get('Address'),
@@ -21,8 +21,8 @@ def list_instances():
                                instances.get('DBInstances')[n].get('EngineVersion'),
                                instances.get('DBInstances')[n].get('MultiAZ'),
                                instances.get('DBInstances')[n].get('DBInstanceStatus')
-                             ) ) 
-      num = num + 1
+                              ) )
+       num = num + 1
 
 def main():
     parser = argparse.ArgumentParser(description='List all the RDS instances')
