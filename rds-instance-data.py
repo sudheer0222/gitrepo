4fd3cbff1f5905eec906d29c072c9316a18ed510
sudheer <30334083+sudheer0222@users.noreply.github.com>
2019-03-14 18:03:52 +0530

    Update rds-instance-data.py

--- rds-instance-data.py
+++ rds-instance-data.py
@@ -6,24 +6,24 @@ import argparse
 
 
 def list_instances():
-   client = boto3.client('rds')
-   instances = client.describe_db_instances() 
-   columns_format="%-3s %-20s %-60s %-12s %-7s %-8s %-8s %-12s"
-   print ( columns_format % ("num", "Identifier", "Endpoint Address", "Class", "BackupRetentionPeriod" , "Engine", "Version", "MultiAZ","Status"))
-   num = 1
-   for n in range(len(instances.get('DBInstances'))):
-      print ( columns_format % (
+    client = boto3.client('rds')
+    instances = client.describe_db_instances() 
+    columns_format="%-3s %-22s %-63s %-12s %-22s %-18s %-8s %-8s %-12s"
+    print (columns_format % ("num", "Identifier", "Endpoint Address", "Class", "BackupRetentionPeriod", "Engine", "Version", "MultiAZ","Status"))
+    num = 1
+    for n in range(len(instances.get('DBInstances'))):
+       print ( columns_format % (
                                num,
                                instances.get('DBInstances')[n].get('DBInstanceIdentifier'),
                                instances.get('DBInstances')[n].get('Endpoint').get('Address'),
                                instances.get('DBInstances')[n].get('DBInstanceClass'),
-                               instances.get('DBInstances')[n].get('BackupRetentionPeriod'),
+							          instances.get('DBInstances')[n].get('BackupRetentionPeriod'),
                                instances.get('DBInstances')[n].get('Engine'),
                                instances.get('DBInstances')[n].get('EngineVersion'),
                                instances.get('DBInstances')[n].get('MultiAZ'),
                                instances.get('DBInstances')[n].get('DBInstanceStatus')
-                             ) ) 
-      num = num + 1
+                              ) )
+       num = num + 1
 
 def main():
     parser = argparse.ArgumentParser(description='List all the RDS instances')
