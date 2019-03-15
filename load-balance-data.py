4977a67da683fb0c1dcf25bf1dbe12dc928b64f6
sudheer <30334083+sudheer0222@users.noreply.github.com>
2019-03-15 12:04:45 +0530

    Update load-balance-data.py

--- load-balance-data.py
+++ load-balance-data.py
@@ -1,9 +1,12 @@
-#ELB Load Balancer script to fetch the data
-
-
+# Load Balancer details
 import boto3
 client = boto3.client('elb')
 balancer = client.describe_load_balancers()
-print("LoadBalancerName                ","|","DNSName                                                               ","|","CanonicalHostedZoneName")
-for i in balancer['LoadBalancerDescriptions']:
-    print(i['LoadBalancerName'],"|",i['DNSName'],"|",i['CanonicalHostedZoneName'])
+columns_format="%-35s %-70s %-50s"
+print(columns_format%("LoadBalancerName","DNSName","CanonicalHostedZoneName"))
+for i in range(len(balancer.get('LoadBalancerDescriptions'))):
+    print(columns_format %(
+                           balancer.get('LoadBalancerDescriptions')[i].get('LoadBalancerName'),
+                           balancer.get('LoadBalancerDescriptions')[i].get('DNSName'),
+                           balancer.get('LoadBalancerDescriptions')[i].get('CanonicalHostedZoneName')
+                           ))
